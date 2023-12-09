9be17742d6c928c6ba0e035794708ebb1e36a2a9
athomas227 <andrewthomas@pursuit.org>
2023-12-09 18:03:56 -0500

    finished exercise

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ClassGreeting from './ClassGreeting';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test</h1>
+      <ClassGreeting name="Yusuke"/>
+    </div>
+  );
+}
+
+export default App;
