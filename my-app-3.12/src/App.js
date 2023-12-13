15068a3f7beb50f0662dcce14ac20e1160dc69df
eaapursuit <elishaabochie-ansah@pursuit.org>
2023-12-13 08:34:32 -0500

    added a ClassGreeting component to my react app

---
+++ my-app-3.12/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import ClassGreeting from './ClassGreeting';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ClassGreeting name='Elisha'/>
+    </div>
+  );
+}
+
+export default App;
