22c1950679a97bebe0dc9d52e874e37cc1c00b12
godcindy <cindygodoy@pursuit.org>
2023-12-15 13:19:17 -0500

    final

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import {ClassGreeting, Greeting} from './ClassGreeting';
+import './App.css';
+
+function App() {
+
+
+  return (
+<div>
+
+  <Greeting />
+</div>
+ 
+  );
+}
+
+export default App;
+
---
+++ src/ClassGreeting.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ClassGreeting extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+const element = <ClassGreeting name="Chico" />;
+function Greeting() {
+  return <h1>{element}</h1>;
+}
+export { ClassGreeting, Greeting };
