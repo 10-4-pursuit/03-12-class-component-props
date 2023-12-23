38420c849f1c3141a9d0898f4878157b33cae378
clairep9 <clairepetitfrere@pursuit.org>
2023-12-23 11:40:02 -0500

    Add ClassGreeting component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassGreeting from './ClassGreeting';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassGreeting name="Claire" />
+    </div>
+  );
+}
+
+export default App;
