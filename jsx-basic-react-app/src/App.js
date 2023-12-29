d8f7874c06238d19dbb8a423ad926d63ce637f61
Azmain Shoeb <azmainshoeb@pursuit.org>
2023-12-28 19:52:02 -0500

    Add ClassGreeting component

---
+++ jsx-basic-react-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ClassGreeting from './ClassGreeting';
+
+function App() {
+  return (
+    <div className="App">
+     <ClassGreeting name="#Knickstape" />  
+    </div>
+  );
+}
+
+export default App;
