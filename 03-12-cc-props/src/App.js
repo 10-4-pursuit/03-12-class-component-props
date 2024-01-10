0453da869053642f90927b5d7824f4563fb8a4ae
bperalta14 <briannaperalta@pursuit.org>
2024-01-10 15:05:02 -0500

    Add ClassGreeting component

---
+++ 03-12-cc-props/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ClassGreeting from './ClassGreeting';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Class Component Props</h1>
+      <ClassGreeting name="Bri" />
+    </div>
+  );
+};
+
+export default App;
