0c0bbea9491dc99066df269c594e39291132a98c
Mikal Din <Mikal Wazeerud-din@pursuit.org>
2024-01-28 09:32:37 -0500

    I passed my name through props

---
+++ 03-12-class-component-props/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ClassGreeting from './ClassGreeting';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ClassGreeting name="Mikals" />
+
+    </div>
+  );
+}
+
+export default App;
