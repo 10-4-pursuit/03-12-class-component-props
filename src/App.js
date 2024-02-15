13a98eaac5d08a2d2d65a838739c1c33f05407a2
AmiraFredericks <amirafredericks@pursuit.org>
2024-02-14 22:37:16 -0500

    Add ClassGreeting component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ClassGreeting from './ClassGreeting';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <ClassGreeting name="World"/>
+    </div>
+  );
+}
+
+export default App;
